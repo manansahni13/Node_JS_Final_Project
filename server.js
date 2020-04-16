97f7d91e6b8075819b30775fed292b6792f251c3
Manan Sahni <manan.sahni@nagarro.com>
2020-04-16 12:35:15 +0530

    set up server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const { db } = require('./db')
+const todoRoute = require('./routes/todos')
+
+const app = express()
+
+const SERVER_PORT = process.env.PORT || 3000
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use('/', express.static(__dirname + '/public'))
+
+app.use('/todos', todoRoute)
+
+db.sync()
+  .then(() => {
+    app.listen(SERVER_PORT)
+  })
+  .catch((err) => {
+    console.error(err)
+  })
